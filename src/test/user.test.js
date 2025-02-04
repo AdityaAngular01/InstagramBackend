6a945a9c19e475659ca82b6577c16ee5faf585f3
AdityaAngular01 <aditya@angularminds.in>
2025-02-04 18:04:23 +0530

    Implement tests for user-related functionality and endpoints

---
+++ src/test/user.test.js
@@ -0,0 +1,186 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+const { app, connectToDb } = require("../index"); // Make sure this is correct
+
+let server;
+let token;
+
+beforeAll(async () => {
+	await connectToDb();
+	server = app.listen();
+	token = await require("./userlogin").getToken(app);
+});
+
+afterAll(async () => {
+	await mongoose.connection.close();
+	server.close();
+});
+
+describe("User Tests", () => {
+
+	//Test Cases for User profile Updations
+	describe("Update User Profile", () => {
+		//Update Full Name of User
+		it("Update Full Name", async () => {
+			const response = await request(app)
+				.put("/user/update/profile")
+				.set("Authorization", `Bearer ${token}`)
+				.send({
+					update: "fullName",
+					fullName: "Aditya Vasant Kalambe",
+				})
+				.expect(200);
+			expect(response.body).toHaveProperty(
+				"message",
+				"User profile updated successfully"
+			);
+		});
+
+		//Update Bio of User
+		it("Update User Bio", async () => {
+			const response = await request(app)
+				.put("/user/update/profile")
+				.set("Authorization", `Bearer ${token}`)
+				.send({
+					update: "bio",
+					bio: "I am a software engineer.",
+				})
+				.expect(200);
+			expect(response.body).toHaveProperty(
+				"message",
+				"User profile updated successfully"
+			);
+		});
+
+		//Update Profile Picture of User
+		it("Update User Profile Picture", async () => {
+			const response = await request(app)
+				.put("/user/update/profile")
+				.set("Authorization", `Bearer ${token}`)
+				.send({
+					update: "profilePicture",
+					profilePicture:
+						"https://static.vecteezy.com/system/resources/previews/036/324/708/large_2x/ai-generated-picture-of-a-tiger-walking-in-the-forest-photo.jpg",
+				})
+				.expect(200);
+			expect(response.body).toHaveProperty(
+				"message",
+				"User profile updated successfully"
+			);
+		});
+	});
+
+	it("User Info", async () => {
+		const response = await request(app)
+			.get("/user/info")
+			.set("Authorization", `Bearer ${token}`)
+			.expect(200);
+
+		expect(response.body).toHaveProperty("_id");
+		expect(response.body).toHaveProperty("username");
+		expect(response.body).toHaveProperty("email");
+		expect(response.body).toHaveProperty("fullName");
+		expect(response.body).toHaveProperty("createdAt");
+		expect(response.body).toHaveProperty("updatedAt");
+		expect(response.body).toHaveProperty("bio");
+		expect(response.body).toHaveProperty("profilePicture");
+		expect(response.body).toHaveProperty("followersCount");
+		expect(response.body).toHaveProperty("followingCount");
+		expect(response.body).toHaveProperty("postsCount");
+	});
+
+	//Follow User and Unfollow User
+	describe("Follow and Unfollow User", () => {
+		const id = "60c5445577606d0015883a35";
+		//Follow User
+		it("Follow User", async () => {
+			const response = await request(app)
+				.put(`/user/follow/${id}`)
+				.set("Authorization", `Bearer ${token}`);
+			expect([200, 404, 500]).toContain(response.status);
+
+			expect([
+				"User followed successfully",
+				"User not found",
+				"Internal Server error",
+			]).toContain(response.body.message);
+		});
+
+		//Unfollow User
+		it("Unfollow User", async () => {
+			const response = await request(app)
+				.put(`/user/unfollow/${id}`)
+				.set("Authorization", `Bearer ${token}`);
+			expect([200, 404, 500]).toContain(response.status);
+			expect([
+				"User unfollowed successfully",
+				"User not found",
+				"Internal Server error",
+			]).toContain(response.body.message);
+		});
+	});
+
+	describe("Follwer and Following List", () => {
+		it("List of Followers", async () => {
+			const response = await request(app)
+				.get("/user/followers")
+				.set("Authorization", `Bearer ${token}`);
+
+			expect([200, 404]).toContain(response.status);
+
+			// expect(Array.isArray(response.body.following)).toBe(true);
+		});
+
+		//List of Following
+		it("List of Following", async () => {
+			const response = await request(app)
+				.get("/user/followings")
+				.set("Authorization", `Bearer ${token}`);
+			expect([200, 404]).toContain(response.status);
+
+			// expect(Array.isArray(response.body.following)).toBe(true);
+		});
+	});
+
+	//Search User
+	describe("Search User", () => {
+		//Search By Username
+		it("Search By Username", async () => {
+			const response = await request(app)
+				.get("/user/search")
+				.send({
+					by: "username",
+					username: "test",
+				})
+				.set("Authorization", `Bearer ${token}`);
+			expect([200, 404, 500]).toContain(response.status);
+
+			// expect(Array.isArray(response.body.users)).toBe(true);
+		});
+
+		//Search By Full Name
+		it("Search By Full Name", async () => {
+			const response = await request(app)
+				.get("/user/search")
+				.send({
+					by: "fullName",
+					fullName: "Aditya Vasant",
+				})
+				.set("Authorization", `Bearer ${token}`);
+
+			expect([200, 404, 500]).toContain(response.status);
+			// expect(Array.isArray(response.body.users)).toBe(true);
+		});
+	});
+
+	// Delete user
+	describe("Delete Acoount", () => {
+		it("Delete Acoount", async () => {
+			const response = await request(app).delete("/user/delete/account").set("Authorization", `Bearer ${token}`);
+			expect([200, 401, 500]).toContain(response.status);
+			expect(["User deleted successfully", "User not found", "Internal server error"]).toContain(
+				response.body.message
+			);
+		});
+	});
+});
