d31a7a2556541965d47029f137bdb2e16104d7c1
AdityaAngular01 <aditya@angularminds.in>
2025-02-04 17:59:56 +0530

    Implement authentication routes for user login and registration

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+const {newUser, login} = require('../controller/auth.controller');
+
+const router = require('express').Router();
+
+const {ValidateJoi, Schemas} = require('../middleware/model.validation');
+
+router.post('/signup', ValidateJoi(Schemas.userSchema), newUser);
+
+router.post('/login', ValidateJoi(Schemas.userLogin), login);
+
+module.exports = router;
