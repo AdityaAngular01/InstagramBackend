c6a4ca5152698fd3ebd275df7db6adca276ee98c
AdityaAngular01 <aditya@angularminds.in>
2025-02-04 18:00:03 +0530

    Implement user routes for profile management and user-related actions

---
+++ src/routes/user.routes.js
@@ -0,0 +1,26 @@
+const { Schemas, ValidateJoi } = require("../middleware/model.validation");
+
+const {
+	getUserInfo,
+	deleteAccount,
+	updateProile,
+	followingList,
+	followersList,
+	searchUser,
+	followUser,
+	unfollowUser
+} = require("../controller/user.controller");
+
+const router = require("express").Router();
+
+router
+	.get("/info", getUserInfo)
+	.get("/followings", followingList)
+	.get("/followers", followersList)
+	.get("/search", ValidateJoi(Schemas.searchUser), searchUser)
+	.delete("/delete/account", deleteAccount)
+	.put("/update/profile", ValidateJoi(Schemas.updateProfile), updateProile)
+	.put("/follow/:userId", followUser)
+	.put("/unfollow/:userId", unfollowUser)
+
+module.exports = router;
