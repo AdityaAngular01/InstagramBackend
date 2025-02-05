5c62008bc76d59fff22977809c12b0f074d896ae
AdityaAngular01 <aditya@angularminds.in>
2025-02-05 18:52:55 +0530

    Implement like model for tracking post likes

---
+++ src/model/like.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const likeSchema = new Schema(
+	{
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		postId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Post",
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+// Index to prevent duplicate likes
+likeSchema.index({ userId: 1, postId: 1 }, { unique: true });
+
+/**
+ * @desc Like a post
+ */
+likeSchema.statics.likePost = async function (userId, postId) {
+	return await this.create({ userId, postId });
+};
+
+/**
+ * @desc Unlike a post
+ */
+likeSchema.statics.unlikePost = async function (userId, postId) {
+	return await this.findOneAndDelete({ userId, postId });
+};
+
+/**
+ * @desc Check if a user has liked a post
+ */
+likeSchema.statics.hasLiked = async function (userId, postId) {
+	return await this.exists({ userId, postId });
+};
+
+/**
+ * @desc Get total likes for a post
+ */
+likeSchema.statics.getLikesCount = async function (postId) {
+	return await this.countDocuments({ postId });
+};
+
+const Like = mongoose.model("Like", likeSchema);
+module.exports = Like;
