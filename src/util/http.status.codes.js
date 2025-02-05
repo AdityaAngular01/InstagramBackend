72390c5c96f5cc8ac3bcd07c32e8b76426a8ef7f
AdityaAngular01 <aditya@angularminds.in>
2025-02-05 18:58:08 +0530

    Update HTTP status codes utility with additional status definitions

--- src/util/http.status.codes.js
+++ src/util/http.status.codes.js
@@ -1,4 +1,4 @@
-const {httpStatusCodesUtils: httpUtil} = require('nodejs-utility-package');
+const { httpStatusCodesUtils: httpUtil } = require("nodejs-utility-package");
 
 const httpStatus = {
 	INTERNAL_SERVER_ERROR: {
@@ -7,18 +7,39 @@ const httpStatus = {
 			httpUtil.HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR
 		),
 	},
-    NOT_FOUND:{
-        CODE: httpUtil.HTTP_STATUS_CODES.NOT_FOUND,
-        MESSAGE: httpUtil.getStatusMessage(
-            httpUtil.HTTP_STATUS_CODES.NOT_FOUND
-        ),
-    },
-    UNAUTHORIZED:{
-        CODE: httpUtil.HTTP_STATUS_CODES.UNAUTHORIZED,
-        MESSAGE: httpUtil.getStatusMessage(
-            httpUtil.HTTP_STATUS_CODES.UNAUTHORIZED
-        ),
-    }
+	NOT_FOUND: {
+		CODE: httpUtil.HTTP_STATUS_CODES.NOT_FOUND,
+		MESSAGE: {
+			BASE: httpUtil.getStatusMessage(
+				httpUtil.HTTP_STATUS_CODES.NOT_FOUND
+			),
+			POST: "Post not found or unauthorized",
+		},
+	},
+	UNAUTHORIZED: {
+		CODE: httpUtil.HTTP_STATUS_CODES.UNAUTHORIZED,
+		MESSAGE: httpUtil.getStatusMessage(
+			httpUtil.HTTP_STATUS_CODES.UNAUTHORIZED
+		),
+	},
+	CREATED: {
+		CODE: httpUtil.HTTP_STATUS_CODES.CREATED,
+		MESSAGE: httpUtil.getStatusMessage(httpUtil.HTTP_STATUS_CODES.CREATED),
+	},
+	FOUND: {
+		CODE: httpUtil.HTTP_STATUS_CODES.FOUND,
+		MESSAGE: httpUtil.getStatusMessage(httpUtil.HTTP_STATUS_CODES.FOUND),
+	},
+	OK: {
+		CODE: httpUtil.HTTP_STATUS_CODES.OK,
+		MESSAGE: httpUtil.getStatusMessage(httpUtil.HTTP_STATUS_CODES.OK),
+	},
+	BAD_REQUEST: {
+		CODE: httpUtil.HTTP_STATUS_CODES.BAD_REQUEST,
+		MESSAGE: httpUtil.getStatusMessage(
+			httpUtil.HTTP_STATUS_CODES.BAD_REQUEST
+		),
+	},
 };
 
 module.exports = httpStatus;
