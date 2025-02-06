66cc9760f646cabc5b38230fd1a744d6542f7e10
AdityaAngular01 <aditya@angularminds.in>
2025-02-06 19:01:27 +0530

    Update user controller with enhanced user management logic

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,6 @@
 const User = require("../model/user.model");
 const FollowerFollowing = require("../model/followerFollowing.model");
+const Post = require("../model/post.model")
 
 const httpStatus = require("../util/http.status.codes");
 const {internalServerError, notFound} = require('./error.controller')
@@ -36,7 +37,7 @@ exports.deleteAccount = async (req, res) => {
         //Remove followers and following records
         await FollowerFollowing.deleteFollowers( _id );
         await FollowerFollowing.deleteFollowings( _id );
-        
+        await Post.deleteMany({userId: _id})
 		res.status(200).json({ message: "User deleted successfully" });
 	} catch (error) {        
 		return internalServerError(req, res, error);
@@ -68,35 +69,6 @@ exports.updateProile = async (req, res) => {
 	}
 };
 
-//Folling List
-exports.followingList = async(req, res)=>{
-    try {
-        const { _id } = req.user;
-        const following = await User.findById(_id).select("following -_id").populate('following', "_id username profilePictire");
-        if(!following){
-			return notFound(req, res, "User not found");
-        }
-        res.status(200).json(following.following);
-    } catch (error) {
-		return internalServerError(req, res, error);
-    }
-}
-
-//Followers List
-exports.followersList = async(req, res)=>{
-    try {
-        const { _id } = req.user;
-        const followers = await User.findById(_id).select("followers -_id").populate('followers', "_id username profilePicture");
-        if(!followers){
-			return notFound(req, res, "User not found");
-        }
-        res.status(200).json(followers.followers);
-    } catch (error) {
-		return internalServerError(req, res, error);
-
-    }
-}
-
 //Serach User
 exports.searchUser = async(req, res)=>{
     try {
@@ -117,82 +89,6 @@ exports.searchUser = async(req, res)=>{
 			return notFound(req, res, "User not found");
         }
         res.status(200).json(users);
-    } catch (error) {
-		return internalServerError(req, res, error);
-
-    }
-}
-
-// Follow User
-exports.followUser = async(req, res)=>{
-    try {
-        const { _id: followerId } = req.user;
-        const { userId: followingId } = req.params;
-
-        const self = await User.findByUserId(followerId);
-        
-        if (!self) {
-			return notFound(req, res, "User not found");
-		}
-        const recipient = await User.findByUserId(followingId);
-        if (!recipient) {
-            return res
-               .status(httpStatus.NOT_FOUND.CODE)
-               .json({ message: "User not found" });
-        }
-
-        const alreadyFollowed = await FollowerFollowing.isFollowing(followerId,followingId);
-        
-        if (alreadyFollowed) {
-			return res
-				.status(409)
-				.json({ message: "You are already following this user" });
-		}
-
-        const follow = await FollowerFollowing.followUser(
-			followerId,
-			followingId
-		);     
-        await self.incrementFollowing();
-        await recipient.incrementFollowers();
-
-        res.status(200).json({ message: "User followed successfully" });
-    } catch (error) {
-		return internalServerError(req, res, error);
-
-    }
-}
-
-// Unfollow User
-exports.unfollowUser = async(req, res)=>{
-    try {
-        const { _id: followerId } = req.user;
-        const { userId: followingId } = req.params;
-
-        const self = await User.findByUserId(followerId);
-		if (!self) {
-			return notFound(req, res, "User not found");
-		}
-		const recipient = await User.findByUserId(followingId);
-		if (!recipient) {
-			return notFound(req, res, "User not found");
-		}
-
-        const alreadyFollowed = await FollowerFollowing.isFollowing(followerId,followingId);
-        if (!alreadyFollowed){
-            return res
-               .status(409)
-               .json({ message: "You are not following this user" });
-        }
-        const follow = await FollowerFollowing.unfollowUser(
-			followerId,
-			followingId
-		);  
-
-		await self.decrementFollowing();
-		await recipient.decrementFollowers();
-
-        res.status(200).json({ message: "User unfollowed successfully" });
     } catch (error) {
 		return internalServerError(req, res, error);
 
