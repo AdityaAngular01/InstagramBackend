7d5cb08ce3bc8080b5ab017167b1b02940e8b508
AdityaAngular01 <aditya@angularminds.in>
2025-02-06 19:19:38 +0530

    Update index.js to include routes for comments, likes, follow-following, media, hashtags, messages, and notifications

--- src/index.js
+++ src/index.js
@@ -20,11 +20,25 @@ app.use(helmet());
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
+const commentRoutes = require("./routes/comment.routes");
+const likeRoutes = require("./routes/like.routes");
+const followerFollowingRoutes = require("./routes/followerFollowing.routes");
+const mediaRoutes = require("./routes/media.routes");
+const hashtagRoutes = require("./routes/hashtag.routes");
+const messageRoutes = require("./routes/message.routes");
+const notificationRoutes = require("./routes/notification.routes");
 
 // Routes
 app.use("/auth", authRoutes);
 app.use("/user", auth, userRoutes);
 app.use("/post", auth, postRoutes);
+app.use("/comment", auth, commentRoutes);
+app.use("/like", auth, likeRoutes);
+app.use("/follow", auth, followerFollowingRoutes);
+app.use("/media", auth, mediaRoutes);
+app.use("/hashtag", auth, hashtagRoutes);
+app.use("/message", auth, messageRoutes);
+app.use("/notification", auth, notificationRoutes);
 
 // Export app without starting the server
 module.exports = { app, connectToDb };
