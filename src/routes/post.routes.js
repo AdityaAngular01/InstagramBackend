c9101692e9afcd694747c188f0b7bcb6a407d244
AdityaAngular01 <aditya@angularminds.in>
2025-02-06 19:24:10 +0530

    Update post routes with additional post management functionalities

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -4,24 +4,14 @@ const {
 	getAllPosts,
 	getPostById,
 	getPostsByUser,
-	getCommentsForPost,
 	deletePost,
-	likePost,
-	unlikePost,
-	commentOnPost,
-	deleteComment,
 } = require("../controller/post.controller");
 
 router
 	.post("/", createPost)
 	.get("/", getAllPosts)
 	.get("/:postId", getPostById)
-	.get("/:postId/comments", getCommentsForPost)
 	.get("/user/:userId", getPostsByUser)
-	.post("/:postId/like", likePost)
-	.post("/:postId/unlike", unlikePost)
-	.post("/:postId/comment", commentOnPost)
-	.delete("/:postId/comment/:commentId", deleteComment)
 	.delete("/:postId", deletePost);
 
 module.exports = router;
