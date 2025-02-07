918637aabb93d24be97e334078719a077e731d12
AdityaAngular01 <aditya@angularminds.in>
2025-02-07 15:26:49 +0530

    Refactor: Validate post existence in commentsOnPost & add updateComment API

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -1,5 +1,8 @@
-const Comment = require('../model/comment.model');
-const Post = require('../model/post.model');
+const Comment = require("../model/comment.model");
+const Post = require("../model/post.model");
+const { notFound, internalServerError } = require("./error.controller");
+const httpUtil = require("../util/http.status.codes");
+const { response } = require("express");
 
 // Comment on a Post
 exports.commentOnPost = async (req, res) => {
@@ -8,15 +11,18 @@ exports.commentOnPost = async (req, res) => {
 		const { text } = req.body;
 		const userId = req.user._id;
 
-		const comment = await Comment.createComment(userId, postId, text);
-
 		const post = await Post.findById(postId);
+		if (!post) {
+			return notFound(req, res, httpUtil.NOT_FOUND.MESSAGE.POST);
+		}
+
+		const comment = await Comment.createComment(userId, postId, text);
 
 		await post.incrementComments();
 
 		res.status(201).json({ success: true, comment });
 	} catch (error) {
-		res.status(500).json({ success: false, message: error.message });
+		return internalServerError(req, res, error);
 	}
 };
 
@@ -24,23 +30,22 @@ exports.commentOnPost = async (req, res) => {
 exports.deleteComment = async (req, res) => {
 	try {
 		const { postId, commentId } = req.params;
-		const userId = req.user._id;
 
-		const comment = await Comment.deleteComment(commentId, userId);
+		const post = await Post.findOne({ _id: postId });
+		
+		
+		if (!post) {
+			return notFound(req, res, httpUtil.NOT_FOUND.MESSAGE.POST);
+		}
+		const comment = await Comment.deleteComment(commentId, req.user._id);
 		if (!comment)
-			return res
-				.status(404)
-				.json({
-					success: false,
-					message: "Comment not found or unauthorized",
-				});
+			return notFound(req, res, httpUtil.NOT_FOUND.MESSAGE.COMMENT);
 
-		const post = await Post.findById(postId);
 		await post.decrementComments();
 
 		res.status(200).json({ success: true, message: "Comment deleted" });
 	} catch (error) {
-		res.status(500).json({ success: false, message: error.message });
+		return internalServerError(req, res, error);
 	}
 };
 
@@ -52,17 +57,38 @@ exports.getCommentsForPost = async (req, res) => {
 
 		res.status(200).json({ success: true, comments });
 	} catch (error) {
-		res.status(500).json({ success: false, message: error.message });
+		return internalServerError(req, res, error);
 	}
 };
 
 // Get Comments Counts
 exports.getCommentsCount = async (req, res) => {
 	try {
-		const {postId} = req.params;
+		const { postId } = req.params;
 		const totalComments = await Comment.getCommentsCount(postId);
 		res.status(200).json({ success: true, totalComments });
 	} catch (error) {
-		res.status(500).json({ success: false, message: error.message });
+		return internalServerError(req, res, error);
 	}
-}
+};
+
+//Update Comment
+exports.updateComment = async (req, res) => {
+    try {
+        const { commentId } = req.params;
+        const userId = req.user._id;
+        const { text } = req.body;
+
+        const comment = await Comment.getCommentByIdAndUserId(commentId, userId);
+
+        if (!comment)
+            return notFound(req, res, httpUtil.NOT_FOUND.MESSAGE.COMMENT);
+		
+        comment.text = text;
+		await comment.save();
+
+        res.status(200).json({ success: true, comment });
+    } catch (error) {
+        return internalServerError(req, res, error);
+    }
+};
