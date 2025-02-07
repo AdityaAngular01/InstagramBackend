d3c0d5e5b70164c2e5ee453e6e5da585b49952aa
AdityaAngular01 <aditya@angularminds.in>
2025-02-07 15:26:49 +0530

    Test: Added various invalid IDs (post, comment, user, etc.) for improved test coverage

--- src/test/util.js
+++ src/test/util.js
@@ -12,5 +12,12 @@ const disconnectMongoDb = async (server) => {
 	server.close();
 };
 
+const invalidIds = {
+	postId: "123456789012345678901234",
+	commentId: "123456789012345678901234",
+	userId: "123456789012345678901234",
+	likeId: "123456789012345678901234",
+};
 
-module.exports = { connectMongoDb, disconnectMongoDb, app };
+
+module.exports = { connectMongoDb, disconnectMongoDb, app , invalidIds};
