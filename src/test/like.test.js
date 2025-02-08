b96a7fedd532eb645da52f2e0775c1f1caa8a99a
AdityaAngular01 <aditya@angularminds.in>
2025-02-08 18:34:59 +0530

    Refined like test - Improved test cases for like functionality

--- src/test/like.test.js
+++ src/test/like.test.js
@@ -1,20 +1,27 @@
 const request = require("supertest");
-const { app, connectMongoDb, disconnectMongoDb } = require("./util");
-
+const {
+	connectMongoDb,
+	disconnectMongoDb,
+	app,
+	getToken,
+	deleteTestAccount,
+	authData,
+} = require("./util");
 let server;
 let token;
 
 beforeAll(async () => {
 	server = await connectMongoDb();
-	token = await require("./userlogin").getToken(app);
+	token = await getToken(authData.user1);
 });
 
 afterAll(async () => {
+	await deleteTestAccount(token);
 	await disconnectMongoDb(server);
 });
 
 describe("Like Test Cases", () => {
-	let postId = "67a4ae93ec3003174f9352b0";
+	let postId = "67a7022c51aeea506141be39";
 
 	it("Like Post", async () => {
 		const response = await request(app)
@@ -24,22 +31,20 @@ describe("Like Test Cases", () => {
 		expect([200, 400, 404]).toContain(response.status);
 	});
 
-    it('Unlike Post', async() => {
-        const response = await request(app)
+	it("Unlike Post", async () => {
+		const response = await request(app)
 			.delete(`/like/${postId}`)
 			.set("Authorization", `Bearer ${token}`);
 
 		expect([200, 400, 404]).toContain(response.status);
-    });
+	});
 
-    it('Get Like Count', async() => {
-        const response = await request(app)
+	it("Get Like Count", async () => {
+		const response = await request(app)
 			.get(`/like/count/${postId}`)
 			.set("Authorization", `Bearer ${token}`);
 
 		expect([200, 400, 404]).toContain(response.status);
-        expect(response.body).toHaveProperty('likesCount');
-    });
-    
-    
+		expect(response.body).toHaveProperty("likesCount");
+	});
 });
