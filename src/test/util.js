310bb7b48fcc4729f708e9db87398ec01d3329e3
AdityaAngular01 <aditya@angularminds.in>
2025-02-08 18:34:59 +0530

    Updated test utilities - Added auth data, post data, getToken & deleteTestAccount

--- src/test/util.js
+++ src/test/util.js
@@ -19,5 +19,78 @@ const invalidIds = {
 	likeId: "123456789012345678901234",
 };
 
+const authData = {
+	user1: {
+		username: "ganeshturate",
+		fullName: "Ganesh Turate",
+		email: "ganeshturate@gmail.com",
+		password: "Ganesh@123",
+	},
+	user2: {
+		username: "sanketparsewar",
+		fullName: "Sanket Parsewar",
+		email: "sanketparsewar@gmail.com",
+		password: "Sanket@123",
+	},
+};
 
-module.exports = { connectMongoDb, disconnectMongoDb, app , invalidIds};
+const post = {
+	url: "https://image.tmdb.org/t/p/w500/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
+	caption: "This is a caption",
+};
+
+//Sign Up And Return token
+const getToken = async(auth)=>{
+	const signUp = await request(app).post("/auth/signup").send(auth);
+
+	const response = await request(app)
+		.post("/auth/login")
+		.send({
+			loginWith: "username",
+			username: auth.username,
+			password: auth.password,
+		});
+    return response.body.token;
+}
+
+//Get User ID
+const getUserID = async(token)=>{
+	const response = await request(app)
+		.get("/user/info")
+		.set("Authorization", `Bearer ${token}`);
+    return response.body._id;
+}
+
+// Delete Account
+const deleteTestAccount = async (token) => {
+	const response = await request(app)
+		.delete("/user/delete/account")
+		.set("Authorization", `Bearer ${token}`);
+	return "";
+};
+
+//Posts
+//Create Post
+// const postId = async(token)=>{
+// 	const newPost = await request(app)
+// 		.post("/post")
+// 		.send({
+// 			imageUrl: post.url,
+// 			caption: post.caption,
+// 		})
+// 		.set("Authorization", `Bearer ${token}`);
+// 	const response = await request(app)
+// }
+
+
+module.exports = {
+	connectMongoDb,
+	disconnectMongoDb,
+	app,
+	invalidIds,
+	getToken,
+	deleteTestAccount,
+	authData,
+	PostData: post,
+	getUserID,
+};
